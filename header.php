ebb0df893bb4fdfe6d1059a4acf9f8edfbe79ea0
sandooo24 <163065420+sandooo24@users.noreply.github.com>
2024-04-29 17:05:43 -0300

    Add files via upload
    tp finalizado

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav class="nav">
+	<h2>Titulo</h2>
+	<ul class="nav-menu">
+		<li class="nav-menu-item"><a href="index.php">Inicio</a></li>
+		<li class="nav-menu-item"><a href="ingreso.php">Nuevo Ingreso</a></li>
+		<li class="nav-menu-item"><a href="selecionarano.php">Estadisticas</a></li>
+	</ul>
+</nav>
